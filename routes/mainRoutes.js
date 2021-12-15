e00047da0db45cbfb7e6557f9931d13df2be7eb8
Opilane TTHK <opilane@tthk.ee>
2021-12-15 13:33:27 +0200

    fínish website

---
+++ routes/mainRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const mainController = require('../controllers/mainController');
+const router = express.Router();
+
+let upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, './images');
+        },
+        filename: function(req, file, cb) {
+            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        }
+    })
+});
+
+router.get('/', mainController.getMainPage);
+
+router.get('/getdate', mainController.getDate);
+
+router.get('/weekday', mainController.getWeekday);
+
+router.post('/', upload.single('userFile'), mainController.postWish);
+
+router.post('/deleteWish', mainController.deleteWish);
+
+module.exports = router;
